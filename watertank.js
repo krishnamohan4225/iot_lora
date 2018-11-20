d981f19a4759f2c1d89ebc8c47f55deac3c56120
krishnamohan4225 <krishna.pvkm@gmail.com>
2018-11-20 19:35:55 +0530

    javascript function to convert base 64 to ascii
    function to convert base 64 value to water tank filled height.
    pass the base64 data and tank height as a arguments

---
+++ watertank.js
@@ -0,0 +1,17 @@
+var data= base64toAscii("AA8OJAEA",28.5)
+function base64toHEX(base64,max) {
+                var raw = atob(base64);
+                var HEX = '';
+                for (i = 0; i < raw.length; i++) {
+                    var _hex = raw.charCodeAt(i).toString(16)
+                    HEX += (_hex.length == 2 ? _hex : '0' + _hex);
+                }
+                var hex= HEX.substr(1,1) + HEX.substr(3,1)+ HEX.substr(5,1);
+                var inches = hex.toLowerCase().split('').reduce((result, ch) =>
+                  result * 16 + '0123456789abcdefgh'.indexOf(ch), 0);
+                 var feet = inches/12;
+                 var filled = max - feet;
+                 filled = filled.toFixed(2);
+                return filled;
+        }
+        
