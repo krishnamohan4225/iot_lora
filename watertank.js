861efed5da5984f2b122dd16861f44b69bfaaf38
krishnamohan4225 <krishna.pvkm@gmail.com>
2018-11-20 21:21:04 +0530

    discarding negative filled values

--- watertank.js
+++ watertank.js
@@ -1,18 +1,21 @@
 //https://jsfiddle.net/krishnakernel/169gv5as/2/
 var data= base64toAscii("AA8OJAEA",28.5)//base 64 data and total tank height from sensor to tank bottom
-function base64toAscii(base64,max) {
-                var raw = atob(base64);
-                var HEX = '';
-                for (i = 0; i < raw.length; i++) {
-                    var _hex = raw.charCodeAt(i).toString(16)
-                    HEX += (_hex.length == 2 ? _hex : '0' + _hex);
-                }
-                var hex= HEX.substr(1,1) + HEX.substr(3,1)+ HEX.substr(5,1);
-                var inches = hex.toLowerCase().split('').reduce((result, ch) =>
-                  result * 16 + '0123456789abcdefgh'.indexOf(ch), 0);
-                 var feet = inches/12; //data in feet
-                 var filled = max - feet;
-                 filled = filled.toFixed(2);
-                return filled;
+ function base64toAscii(base64, max) {
+            var raw = atob(base64);
+            var HEX = '';
+            for (i = 0; i < raw.length; i++) {
+                var _hex = raw.charCodeAt(i).toString(16)
+                HEX += (_hex.length == 2 ? _hex : '0' + _hex);
+            }
+            var hex = HEX.substr(1, 1) + HEX.substr(3, 1) + HEX.substr(5, 1);
+            var inches = hex.toLowerCase().split('').reduce((result, ch) =>
+                result * 16 + '0123456789abcdefgh'.indexOf(ch), 0);
+            var feet = inches / 12; //data in feet
+            var filled = max - feet;
+            filled = filled.toFixed(2);
+            if (filled < 0) //to discard negative values
+            {
+                filled = 0;
+            }
+            return filled;
         }
-        
